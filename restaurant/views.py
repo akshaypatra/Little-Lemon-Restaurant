e0bb9b5b598e3665c8f2a98b2c8cccc8f81e7057
Akshay Patra <akshay.patra114@gmail.com>
2024-07-14 11:53:36 +0530

    completed the website with frontent and backend

---
+++ restaurant/views.py
@@ -0,0 +1,37 @@
+from django.http import HttpResponse
+from django.shortcuts import render,get_object_or_404
+from .forms import BookingForm
+from .models import Menu
+
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def book(request):
+    form = BookingForm()
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {'form':form}
+    return render(request, 'book.html', context)
+
+# Add your code here to create new views
+
+def menu(request):
+    menu_data=Menu.objects.all()
+    main_data={"menu":menu_data}
+    return  render(request,"menu.html",main_data)
+
+def display_menu_items(request,pk=None):
+    if pk:
+        menu_item = get_object_or_404(Menu, pk=pk)
+    else:
+        menu_item="No item available"
+    return render(request,"menu_item.html",{"menu_item":menu_item})
+    
